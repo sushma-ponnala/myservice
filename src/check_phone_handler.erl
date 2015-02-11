d0c6e84ff9bdb8b270ba0a325cdaa378ad384acb
Sushma Ponnala <sushmap@ybrantinc.com>
2015-02-11 13:21:25 +0530

    checkphone

--- src/check_phone_handler.erl
+++ src/check_phone_handler.erl
@@ -1,7 +1,4 @@
-%% Feel free to use, reuse and abuse the code in this file.
-
-%% @doc Hello world handler.
--module(checkphone_handler).
+-module(check_phone_handler).
 
 -export(
   [ init/3, 
@@ -17,40 +14,31 @@ init(_Transport, _Req, []) -> {upgrade, protocol, cowboy_rest}.
 
 terminate(_Reason, _Req, _State) -> ok.
 
-allowed_methods(Req, State) -> 
-	% io:format("allowed_methods\n\n\n"),
+allowed_methods(Req, State) ->
 	{[<<"POST">>], Req, State}.
 
-content_types_accepted(Req, State) -> 
-	% io:format("content_types_accepted\n\n\n"),
+content_types_accepted(Req, State) ->
 	{[  
 		{<<"application/x-www-form-urlencoded">>, handle_request},
 		{<<"application/json">>, handle_request}
 		], Req, State}.
 
 handle_request(Req, State) ->
-	% io:format("handle_request\n\n"),
     {Method, Req2} = cowboy_req:method(Req),
 	process_request(Method, Req2, State).
 
 process_request(<<"POST">>, Req, State) ->
-	% io:format("post process_request \n\n\n"),
-    % Body = <<"{\"rest\": \"Hello POST World!\"}">>,
-
     {ok, PostVals, Req2} = cowboy_req:body_qs(Req),
     % TODO: Perform form validations
 	UserName = binary_to_list(proplists:get_value(<<"userName">>, PostVals)),
-	Phone = binary_to_list(proplists:get_value(<<"phone">>, PostVals)),
-	
-	Result = emysql:execute(hello_pool, "SELECT PHONE FROM lycusers	WHERE USERNAME = '"++ UserName ++"' AND
-				PHONE = '"++Phone++"'		
+	Phone 	 = binary_to_list(proplists:get_value(<<"phone">>, PostVals)),
+	Result   = emysql:execute(hello_pool, "SELECT PHONE, USERNAME FROM lycusers WHERE PHONE = '"++Phone++"' and USERNAME = '"++UserName++"'	
 				"),
-	% Id = integer_to_list(emysql:insert_id(Result1)),
-	% % % TODO: check is row inserted successfully
- %    Result2 = emysql:execute(hello_pool, "SELECT FIRSTNAME, LASTNAME FROM lycusers WHERE ID = '"++Id++"'"),
-    % TODO: check if row exist
-    JSON = emysql:as_json(Result),
-    Body = jsx:encode(JSON),
+    JSON   = emysql:as_json(Result),
+    Output = jsx:encode(JSON),
+    Body   = "{\"status\": 0,
+    		   \"message\":'"++binary_to_list(Output)++"'
+    		  }",
 	process_response("PRESET", Body, Req2, State, 200).
 
 process_response("PRESET", Body, Req, State, StatusCode)->
