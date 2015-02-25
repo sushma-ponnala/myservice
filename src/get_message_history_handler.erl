dce7e49ea36918940ca0976dd7259d286a7e188c
sushma <sushmap@ybrantinc.com>
2015-02-25 18:25:01 +0530

    get_message_history in process

--- src/get_message_history_handler.erl
+++ src/get_message_history_handler.erl
@@ -32,29 +32,27 @@ handle_request(Req, State) ->
 	process_request(Method, Req2, State).
 
 process_request(<<"POST">>, Req, State) ->
-    % {{Year,Month,Day},{Hour,Min,Sec}} = {date(),time()},
 	{ok, PostVals, Req2} = cowboy_req:body_qs(Req),
-    % TODO: Getting form values
 	Sender = binary_to_list(proplists:get_value(<<"sender">>, PostVals)),	
-	
 	% Getting Sender ID
 	Sender_id = emysql:execute(hello_pool, "SELECT ID FROM lycusers WHERE USERNAME = '"++Sender++"' "), 
-    [[{<<"ID">>,SenderID}]] = emysql:as_proplist(Sender_id),
-    % io:format("SELECT ID FROM lycusers WHERE USERNAME = '"++Sender++"' "), 
+    [[{<<"ID">>,Sender_Id}]] = emysql:as_proplist(Sender_id),
+    % io:format("~p ~n",[Sender_Id]), 
+    Check  = emysql:execute(hello_pool, "select rec.receiver_id, lu.username as sender, rec.username as receiver, rec.sender_id, rec.msgType,rec.localPathSender,rec.localPathReceiver,rec.remotePath,rec.mapLat,rec.mapLong,rec.msgDate,rec.msgTime,rec.msgTz,rec.msgStatus,lu.username from (select lycmessages.id, receiver_id, lycusers.username, sender_id,lycmessages.msg_type as msgType,lycmessages.local_path_sender as localPathSender,lycmessages.local_path_receiver as localPathReceiver,lycmessages.remote_path as remotePath,lycmessages.map_lat as mapLat,lycmessages.map_long as mapLong,lycmessages.msg_date as msgDate,lycmessages.msg_time as msgTime,lycmessages.msg_tz as msgTz,lycmessages.msg_status as msgStatus from lycmessages JOIN lycusers ON lycusers.id = lycmessages.receiver_id where lycmessages.sender_id = '"++integer_to_list(Sender_Id)++"') as rec JOIN lycusers lu where rec.sender_id = lu.id"),
 
-    % Result = emysql:execute(hello_pool, "SELECT lu.*,lm.* FROM `lycusers` lu,`lycmessages` lm WHERE lu.`ID` = lm.`SENDER_ID` "),
-    {result_packet,_,_,IdContent,_} = emysql:execute(hello_pool, "SELECT * FROM lycmessages WHERE SENDER_ID = '"++integer_to_list(SenderID)++"'"),
-
-    % io:format("SELECT ID FROM lycmessages WHERE SENDER_ID = '"++integer_to_list(SenderID)++"'"), 
-    io:format("message history count-->:~p~n",[IdContent]), 
-    
-	Body = case [IdContent] of
-        [_] ->
-            "{\"data exist\"}";
-        [] ->            
-            "{\"data does not exist\"}"
-        
-    end,
+    % Check  = emysql:execute(hello_pool, "SELECT APP_MSG_ID, SESSID, RECEIVER_ID, MSG_TYPE, MSG, LOCAL_PATH_SENDER, LOCAL_PATH_RECEIVER, REMOTE_PATH, MAP_LAT, MAP_LONG, MSG_DATE, MSG_TIME, MSG_TZ, MSG_STATUS  FROM lycmessages WHERE SENDER_ID = '"++integer_to_list(Sender_Id)++"'"),
+    Check_Json = emysql:as_json(Check),
+    io:format("Check_Json :~p ~n",[Check_Json]),
+    Add  = jsx:encode([Check_Json]),
+    io:format("Add :~p ~n",[Add]),
+    Body = "{\"status\": 0,
+             \"message\": '"++ binary_to_list(Add) ++"',
+             \"telegram\": true,
+             \"userData\": null,
+             \"groupContacts\": null,
+             \"userGroups\": null
+            }",
+     io:format("body :~p ~n",[Body]),       
 process_response("PRESET", Body, Req2, State, 200).
 
 process_response("PRESET", Body, Req, State, StatusCode)->
